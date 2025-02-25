18857e223bf4571dd558f52cd5cafbdc35c5c25b
Prem Mishra <138608347+Premvkmishra@users.noreply.github.com>
2025-02-25 08:17:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -289,4 +289,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    port = int(os.environ.get("PORT", 8080))  # Use Railway's port
+    socketio.run(app, host="0.0.0.0", port=port)
